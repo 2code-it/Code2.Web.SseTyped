8ba962bff818291f3ab82a9aaad090e7fffe5597
2code <2mail2code@gmail.com>
2024-12-01 14:26:01 +0100

    add return self to extension methods

--- src/Code2.Web.SseTyped/SseExtensions.cs
+++ src/Code2.Web.SseTyped/SseExtensions.cs
@@ -6,35 +6,40 @@ namespace Code2.Web.SseTyped
 {
 	public static class SseExtensions
 	{
-		public static void AddSseTyped(this IServiceCollection services)
+		public static IServiceCollection AddSseTyped(this IServiceCollection services)
 		{
 			services.AddSingleton<ISseConnectionManager, SseConnectionManager>();
 			services.AddSingleton<ISseService, SseService>();
+			return services;
 		}
 
-		public static void UseSseTyped(this IApplicationBuilder app)
+		public static IApplicationBuilder UseSseTyped(this IApplicationBuilder app)
 		{
 			app.UseSseTyped(SseMiddleware.GetDefaultOptions());
+			return app;
 		}
 
-		public static void UseSseTyped(this IApplicationBuilder app, Action<SseMiddlewareOptions> action)
+		public static IApplicationBuilder UseSseTyped(this IApplicationBuilder app, Action<SseMiddlewareOptions> action)
 		{
 			SseMiddlewareOptions options = SseMiddleware.GetDefaultOptions();
 			action(options);
 			app.UseSseTyped(options);
+			return app;
 		}
 
-		public static void UseSseTyped(this IApplicationBuilder app, string[]? allowedTypeNames = null, string? rootPath = null)
+		public static IApplicationBuilder UseSseTyped(this IApplicationBuilder app, string[]? allowedTypeNames = null, string? rootPath = null)
 		{
 			SseMiddlewareOptions options = SseMiddleware.GetDefaultOptions();
 			if (!(allowedTypeNames is null)) options.AllowedTypeNames = allowedTypeNames;
 			if (!(rootPath is null)) options.RootPath = rootPath;
 			app.UseSseTyped(options);
+			return app;
 		}
 
-		public static void UseSseTyped(this IApplicationBuilder app, SseMiddlewareOptions options)
+		public static IApplicationBuilder UseSseTyped(this IApplicationBuilder app, SseMiddlewareOptions options)
 		{
 			app.UseMiddleware<SseMiddleware>(options);
+			return app;
 		}
 	}
 }
